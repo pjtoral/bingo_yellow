3450037186219d96de6cae7ff45929307be5bb98
Jay Emman <jaydegamo22@gmail.com>
2024-02-29 16:15:07 +0800

    new commit

--- lib/ui/views/table/table_viewmodel.dart
+++ lib/ui/views/table/table_viewmodel.dart
@@ -2,48 +2,54 @@ import 'dart:math';
 
 import 'package:stacked/stacked.dart';
 
-class TableViewModel extends BaseViewModel{
-  late List<List<int>> tableNumbers;
+class TableViewModel extends BaseViewModel {
+  late List<List<int>> numbers;
   late List<List<bool>> selectedCells;
+  late List<List<bool>> singleCellSelected; 
 
   TableViewModel() {
-    _initTable();
-  }
-
-  void _initTable() {
-    tableNumbers = List.generate(5, (_) => List.filled(5, 0));
+    numbers = generateTableData();
     selectedCells = List.generate(5, (_) => List.filled(5, false));
-
-    final List<int> firstColumnNumbers = _generateRandomNumbers(1, 15, 5);
-    final List<int> secondColumnNumbers = _generateRandomNumbers(16, 30, 5);
-    final List<int> thirdColumnNumbers = _generateRandomNumbers(31, 45, 4); 
-    final List<int> fourthColumnNumbers = _generateRandomNumbers(46, 60, 5);
-    final List<int> fifthColumnNumbers = _generateRandomNumbers(61, 75, 5);
-
-    for (int i = 0; i < 5; i++) {
-      tableNumbers[i][0] = firstColumnNumbers[i];
-      tableNumbers[i][1] = secondColumnNumbers[i];
-      if (i < 2) {
-        tableNumbers[i][2] = thirdColumnNumbers[i];
-      } else if (i > 2) { 
-        tableNumbers[i][2] = thirdColumnNumbers[i - 1];
-      }
-      tableNumbers[i][3] = fourthColumnNumbers[i];
-      tableNumbers[i][4] = fifthColumnNumbers[i];
-    }
+    singleCellSelected = List.generate(5, (_) => List.filled(5, false));
   }
 
-  List<int> _generateRandomNumbers(int min, int max, int count) {
+  List<int> generateRandomNumbers(int min, int max, int count) {
     final random = Random();
     final Set<int> randomNumbers = {};
-    while (randomNumbers.length < count) {
+
+    while (randomNumbers.length != count) {
       randomNumbers.add(min + random.nextInt(max - min + 1));
     }
+
     return randomNumbers.toList();
   }
 
-  void toggleCellSelection(int row, int col) {
-    if (row == 2 && col == 2) return; 
-    selectedCells[row][col] = !selectedCells[row][col];
+  List<List<int>> generateTableData() {
+    final List<int> firstColumnNumbers = generateRandomNumbers(1, 15, 5);
+    final List<int> secondColumnNumbers = generateRandomNumbers(16, 30, 5);
+    final List<int> thirdColumnNumbers = generateRandomNumbers(31, 45, 5);
+    final List<int> fourthColumnNumbers = generateRandomNumbers(46, 60, 5);
+    final List<int> fifthColumnNumbers = generateRandomNumbers(61, 75, 5);
+
+    return [
+      firstColumnNumbers,
+      secondColumnNumbers,
+      thirdColumnNumbers,
+      fourthColumnNumbers,
+      fifthColumnNumbers,
+    ];
   }
+
+  // Method to update the selected state for a single cell
+  void toggleSingleCellSelection(int row, int col) {
+    if (row == 2 && col == 2) return; // Ignore center cell "Free Space"
+    selectedCells[row][col] = !selectedCells[row][col];
+
+    // Check if a single cell is selected
+    singleCellSelected[row][col] = selectedCells[row][col] &&
+        selectedCells[row].where((isSelected) => isSelected).length == 1 &&
+        List.generate(5, (index) => selectedCells[index][col]).where((isSelected) => isSelected).length == 1;
+  }
+
+  
 }
