4e81eb6592f85a7dcc4d5f6ca7a78a65fa3d8a3d
Paul John toral <pauljohnramireztoral@gmail.com>
2024-02-29 16:57:42 +0800

    added icons

--- lib/ui/views/bingo/bingo_view.dart
+++ lib/ui/views/bingo/bingo_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:stacked/stacked.dart';
 import 'bingo_viewmodel.dart';
@@ -26,35 +27,61 @@ class BingoView extends StackedView<BingoViewModel> {
         child: Center(
           child: Column(
             children: <Widget>[
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(10.0),
-                  boxShadow: const [
-                    BoxShadow(
-                      color:
-                          Colors.amber, // Amber glow color// Spread of the glow
-                      spreadRadius: 7.0, // How much the glow extends
+              Stack(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    SizedBox(
+                      height: 150,
                     ),
-                    BoxShadow(
-                      color: Colors.white, // Black border color
-                      blurRadius: 2.0, // Spread of the border
-                      spreadRadius: 0.0, // Border has no spread
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors
+                                    .amber, // Amber glow color// Spread of the glow
+                                spreadRadius: 7.0, // How much the glow extends
+                              ),
+                              BoxShadow(
+                                color: Colors.white, // Black border color
+                                blurRadius: 2.0, // Spread of the border
+                                spreadRadius: 0.0, // Border has no spread
+                              ),
+                            ],
+                          ),
+                          child: const Padding(
+                              padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
+                              child: Text(
+                                'BINGO',
+                                style: TextStyle(fontSize: 50),
+                              )),
+                        ),
+                      ],
+                    )
+                  ]),
+                  Positioned(
+                    left: -20, // Positioned on the left
+                    top: -15, // Positioned on the top
+                    child: Image.asset(
+                      'assets/1.png', // Path to your image in the assets folder
+                      width: 100, // Set width of the image
+                      height: 100, // Set height of the image
                     ),
-                  ],
-                ),
-                child: const Padding(
-                    padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
-                    child: Text(
-                      'BINGO',
-                      style: TextStyle(fontSize: 50),
-                    )),
-              ),
-              const SizedBox(
-                height: 30,
+                  ),
+                  Positioned(
+                    bottom: -5, // Positioned on the left
+                    right: -25, // Positioned on the top
+                    child: Image.asset(
+                      'assets/2.png', // Path to your image in the assets folder
+                      width: 100, // Set width of the image
+                      height: 100, // Set height of the image
+                    ),
+                  ),
+                ],
               ),
               const TableView(),
             ],
