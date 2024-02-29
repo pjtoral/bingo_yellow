b32650779e88e56aae029541c5e51d593c2ab897
Jezzer John Fernandez <jfernandez@highleap.com.ph>
2024-03-01 00:37:44 +0800

    clean and fixed to view

--- lib/ui/views/bingo/bingo_view.dart
+++ lib/ui/views/bingo/bingo_view.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:bingo_yellow/themes/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 import 'bingo_viewmodel.dart';
 import 'package:bingo_yellow/ui/views/table/table_view.dart';
@@ -21,128 +18,74 @@ class BingoView extends StackedView<BingoViewModel> {
           icon: const Icon(Icons.arrow_back),
           onPressed: viewModel.navigateToHome,
         ),
-        title: const Text('Bingo Card'),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/7.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: Center(
           child: Column(
-            children: [
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 25.0, right: 25.0),
-                child: Center(
-                  child: Column(
-                    children: <Widget>[
-                      Stack(
-                        children: [
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                SizedBox(
-                                  height: 150,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Expanded(
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                          border: Border.all(
-                                              color: Colors.black, width: 2),
-                                          borderRadius:
-                                              BorderRadius.circular(10.0),
-                                          boxShadow: const [
-                                            BoxShadow(
-                                              color: Colors
-                                                  .amber, // Amber glow color// Spread of the glow
-                                              spreadRadius:
-                                                  7.0, // How much the glow extends
-                                            ),
-                                            BoxShadow(
-                                              color: Colors
-                                                  .white, // Black border color
-                                              blurRadius:
-                                                  2.0, // Spread of the border
-                                              spreadRadius:
-                                                  0.0, // Border has no spread
-                                            ),
-                                          ],
-                                        ),
-                                        child: Padding(
-                                            padding: const EdgeInsets.fromLTRB(
-                                                70, 10, 70, 10),
-                                            child: RichText(
-                                              text: TextSpan(
-                                                children: [
-                                                  for (int i = 0;
-                                                      i < 'BINGO'.length;
-                                                      i++)
-                                                    TextSpan(
-                                                      text: 'BINGO'[i],
-                                                      style: TextStyle(
-                                                        fontSize: 55,
-                                                        fontWeight:
-                                                            FontWeight.w900,
-                                                        color:
-                                                            getColorForLetter(
-                                                                i),
-                                                        shadows: [
-                                                          Shadow(
-                                                            color: Colors.black
-                                                                .withOpacity(
-                                                                    1), // Shadow color
-                                                            offset: const Offset(
-                                                                3,
-                                                                3), // Shadow offset
-                                                            blurRadius:
-                                                                0, // Shadow blur radius
-                                                          ),
-                                                        ],
-                                                      ),
-                                                    ),
-                                                ],
-                                              ),
-                                            )),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ]),
-                          Positioned(
-                            left: -20, // Positioned on the left
-                            top: -15, // Positioned on the top
-                            child: Image.asset(
-                              'assets/1.png', // Path to your image in the assets folder
-                              width: 100, // Set width of the image
-                              height: 100, // Set height of the image
-                            ),
+            children: <Widget>[
+              Stack(
+                children: [
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    const SizedBox(
+                      height: 150,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors
+                                    .amber, // Amber glow color// Spread of the glow
+                                spreadRadius: 7.0, // How much the glow extends
+                              ),
+                              BoxShadow(
+                                color: Colors.white, // Black border color
+                                blurRadius: 2.0, // Spread of the border
+                                spreadRadius: 0.0, // Border has no spread
+                              ),
+                            ],
                           ),
-                          Positioned(
-                            bottom: -5, // Positioned on the left
-                            right: -25, // Positioned on the top
-                            child: Image.asset(
-                              'assets/2.png', // Path to your image in the assets folder
-                              width: 100, // Set width of the image
-                              height: 100, // Set height of the image
-                            ),
-                          ),
-                        ],
-                      ),
-                      const TableView(),
-                    ],
+                          child: const Padding(
+                              padding: EdgeInsets.fromLTRB(70, 10, 70, 10),
+                              child: Text(
+                                'BINGO',
+                                style: TextStyle(fontSize: 50),
+                              )),
+                        ),
+                      ],
+                    )
+                  ]),
+                  Positioned(
+                    left: -20, // Positioned on the left
+                    top: -15, // Positioned on the top
+                    child: Image.asset(
+                      'assets/1.png', // Path to your image in the assets folder
+                      width: 100, // Set width of the image
+                      height: 100, // Set height of the image
+                    ),
                   ),
-                ),
+                  Positioned(
+                    bottom: -5, // Positioned on the left
+                    right: -25, // Positioned on the top
+                    child: Image.asset(
+                      'assets/2.png', // Path to your image in the assets folder
+                      width: 100, // Set width of the image
+                      height: 100, // Set height of the image
+                    ),
+                  ),
+                ],
               ),
+              const TableView(),
             ],
-          )),
+          ),
+        ),
+      ),
     );
   }
 
--- lib/ui/views/bingo/bingo_viewmodel.dart
+++ lib/ui/views/bingo/bingo_viewmodel.dart
@@ -1,91 +1,9 @@
-import 'dart:math';
-
 import 'package:bingo_yellow/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class BingoViewModel extends BaseViewModel {
-final _navigationService = locator<NavigationService>();
-
-  
-  late List<List<int>> tableNumbers;
-  late List<List<bool>> selectedCells;
-
-  void initState() {
-    tableNumbers = List.generate(5, (_) => List.filled(5, 0));
-    selectedCells = List.generate(5, (_) => List.filled(5, false));
-    final List<int> firstColumnNumbers = _generateRandomNumbers(1, 15, 5);
-    final List<int> secondColumnNumbers = _generateRandomNumbers(16, 30, 5);
-    final List<int> thirdColumnNumbers = _generateRandomNumbers(31, 45, 4); // Adjusted for the free space
-    final List<int> fourthColumnNumbers = _generateRandomNumbers(46, 60, 5);
-    final List<int> fifthColumnNumbers = _generateRandomNumbers(61, 75, 5);
-    for (int i = 0; i < 5; i++) {
-      tableNumbers[i][0] = firstColumnNumbers[i];
-      tableNumbers[i][1] = secondColumnNumbers[i];
-      if (i < 2) {
-        tableNumbers[i][2] = thirdColumnNumbers[i];
-      } else if (i > 2) {
-        tableNumbers[i][2] = thirdColumnNumbers[i - 1];
-      }
-      tableNumbers[i][3] = fourthColumnNumbers[i];
-      tableNumbers[i][4] = fifthColumnNumbers[i];
-    }
-  }
-  List<int> _generateRandomNumbers(int min, int max, int count) {
-    final random = Random();
-    final Set<int> randomNumbers = {};
-    while (randomNumbers.length < count) {
-      randomNumbers.add(min + random.nextInt(max - min + 1));
-    }
-    return randomNumbers.toList();
-  }
-  bool checkWinCondition() {
-    // Check if all non-free cells are selected
-    for (int i = 0; i < 5; i++) {
-      for (int j = 0; j < 5; j++) {
-        if (i != 2 || j != 2) {
-          if (!selectedCells[i][j]) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  } 
-  
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  final _navigationService = locator<NavigationService>();
   void navigateToHome() {
     _navigationService.back();
   }
