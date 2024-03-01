1e109998eb35408536bd771e920c91556db69542
Relevie Abalahon <s.abalahon.relevie@gmail.com>
2024-03-01 10:08:09 +0800

    info alert and home view edited

--- lib/ui/dialogs/info_alert/info_alert_dialog_model.dart
+++ lib/ui/dialogs/info_alert/info_alert_dialog_model.dart
@@ -1,3 +1,16 @@
+import 'package:bingo_yellow/app/app.locator.dart';
+import 'package:bingo_yellow/app/app.router.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class InfoAlertDialogModel extends BaseViewModel {}
+class InfoAlertDialogModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  InfoAlertDialogModel(DialogRequest request);
+
+  Future<void> navigateToBingo() async {
+    await _navigationService.navigateToBingoView();
+  }
+
+ 
+}
